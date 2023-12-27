592198404ea840cdc676bf487f2e1006a87a84b2
Selçuk Taştekin <77862040+tastekinselcuk@users.noreply.github.com>
2023-12-27 18:03:49 +0300

    Update taskService.js
    task service updated for render server

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8085/api/task';
+const baseURL = 'https://pomodoro-app-ulpj.onrender.com/api/task';
 
 const taskService = {
   getAllTasks() {
